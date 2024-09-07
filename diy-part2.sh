84d467471bc85d4864b58b18715863bc4c77ecd2
YoKv <ez4tzl@hotmail.com>
2024-09-07 13:12:06 +0800

    config

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,11 +18,6 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-#svn co https://github.com/linkease/istore/trunk/luci package/istore
-#svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-#rm -rf package/istore/.svn
-#sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci-app-store/Makefile
-#sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci-app-store/luasrc/controller/store.lua
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
